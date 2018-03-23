c133d46f2e5bbdb2b290ff7450372c850cad337f
lampv <lampvhy@gmail.com>
2018-03-24 01:19:38 +0700

    adding more abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,16 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    
+
     if user.role?(:admin)
       can :manage, :all
+    elsif user.role?(:moderator)
+      can :create, Project
+      can :read, Project
+    elsif user.role?(:user)
+      can :read, Project
     end
+   
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
